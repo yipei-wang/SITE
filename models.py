8fd28d8790f1cd82fb892f0c329631ab092d4b06
Yipei Wang <wang4865@purdue.edu>
2022-08-04 01:56:31 -0400

    add models.py and utils.py

---
+++ models.py
@@ -0,0 +1,195 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+class BasicBlock(nn.Module):
+
+    def __init__(self, in_planes, planes, stride = 1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes,
+                               planes,
+                               kernel_size = 3,
+                               stride = stride,
+                               padding = 1,
+                               bias = False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, 
+                               planes, 
+                               kernel_size = 3, 
+                               stride = 1,
+                               padding = 1,
+                               bias = False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        
+        self.shortcut = nn.Sequential()
+        if stride !=1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes,
+                          planes, 
+                          kernel_size = 1,
+                          stride = stride,
+                          bias = False),
+                nn.BatchNorm2d(planes))
+            
+    def forward(self, x):
+        out = torch.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = torch.relu(out)
+        return out
+    
+    
+class ResNet(nn.Module):
+    def __init__(self, 
+                 BasicBlock, 
+                 num_classes = 10, 
+                 num_Blocks = [2,2,2,2]):
+        
+        super(ResNet, self).__init__()
+        self.inchannel = 16
+        self.conv1 = nn.Sequential(nn.Conv2d(3, 
+                                             self.inchannel, 
+                                             kernel_size = 3,
+                                             stride = 1,
+                                             padding = 1,
+                                             bias = False),
+                                   nn.BatchNorm2d(self.inchannel),
+                                   nn.ReLU())
+        self.layer1 = self.make_layer(BasicBlock, 
+                                      2*self.inchannel, 
+                                      num_Blocks[0], 
+                                      stride = 1)
+        self.layer2 = self.make_layer(BasicBlock, 
+                                      2*self.inchannel, 
+                                      num_Blocks[1], 
+                                      stride = 2)
+        self.layer3 = self.make_layer(BasicBlock, 
+                                      2*self.inchannel, 
+                                      num_Blocks[2], 
+                                      stride = 2)
+        self.layer4 = self.make_layer(BasicBlock, 
+                                      2*self.inchannel, 
+                                      num_Blocks[3], 
+                                      stride = 2)
+        self.conv2 = nn.Conv2d(256, 10, kernel_size = 3, stride = 1, padding = 1)
+        self.bn1 = nn.BatchNorm2d(10)
+        self.fc = nn.Linear(10, 10)
+        
+    def make_layer(self, BasicBlock, channels, num_Blocks, stride):
+        strides = [stride] + [1] * (num_Blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(BasicBlock(self.inchannel,
+                                        channels,
+                                        stride))
+            self.inchannel = channels
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.bn1(self.conv2(out))
+        feature = torch.tanh(out)
+        out = F.avg_pool2d(feature, 16)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        
+        return feature, out
+    
+    
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(128, 128, 3, 2, 1, output_padding = 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(128, 128, 3, 2, 1, output_padding = 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(128, 128, 3, 2, 1, output_padding = 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(128, 10, 3, 2, 1, output_padding = 1),
+            nn.BatchNorm2d(10),
+            nn.Tanh(),
+            nn.Flatten(),
+        )
+        
+    def forward(self, x):
+        return self.main(x)
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self.extractor = nn.Sequential(
+            nn.Conv2d(10, 128, 3, 2, 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.Conv2d(128, 128, 3, 2, 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.Conv2d(128, 128, 3, 2, 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.Conv2d(128, 128, 3, 2, 1),
+            nn.LeakyReLU()
+        )
+        
+        self.generator = nn.ModuleList([Generator().to(device) for i in range(10)])
+        
+    def forward(self, x):
+        feature = self.extractor(x)
+        W = self.generator[0](feature).view(feature.shape[0], 1, 2560)
+        Y = torch.matmul(W, x.view(feature.shape[0], 2560, 1))
+        Y = Y.view(feature.shape[0], 1)
+        
+        for i in range(1, 10):
+            w = self.generator[i](feature).view(feature.shape[0], 1, 2560)
+            y = torch.matmul(w, x.view(feature.shape[0], 2560, 1))
+            y = y.view(feature.shape[0], 1)
+            
+            W = torch.cat([W, w], dim = 1)
+            Y = torch.cat([Y, y], dim = 1)
+
+        W = W.view(feature.shape[0], 10, 10, 16, 16)
+        
+        return W, Y
+    
+
+class SITE(nn.Module):
+    def __init__(self):
+        super(SITE, self).__init__()
+        self.backbone = ResNet(BasicBlock).to(device)
+        self.site = Classifier().to(device)
+        
+    def forward(self, x):
+        feature, _ = self.backbone(x)
+        _, pred = self.site(feature)
+        return pred
+    
+    def get_explanation(self, x, target_class):
+        feature, _ = self.backbone(x)
+        W, _ = self.site(feature)
+        explanation = (feature.unsqueeze(1)*W).mean(2).squeeze()[target_class]
+        explanation = F.interpolate(explanation[None, None], size = (128, 128), mode = 'bilinear', align_corners = True).squeeze()
+        return explanation
+
+
+if __name__ == '__main__':
+    model = ResNet(BasicBlock).to(device)
+    G = Classifier().to(device)
+    site = SITE().to(device)
+    print('Models are built!')
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,84 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+
+## functions
+
+# show batch images
+def imshow(img,
+        nrow = 10,
+        figsize = (10, 10),
+        save = [False, None]):
+    npimg = torchvision.utils.make_grid(torch.clamp(img, 0,1).cpu().detach(), nrow = nrow)
+    plt.figure(figsize = figsize)
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.axis('off')
+    plt.show()
+
+
+# denormalize normalized images
+def denorm(x):
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    xx = x.clone().detach().to(x.device)
+    xx[:, 0, :, :] = x[:, 0, :, :] * std[0] + mean[0]
+    xx[:, 1, :, :] = x[:, 1, :, :] * std[1] + mean[1]
+    xx[:, 2, :, :] = x[:, 2, :, :] * std[2] + mean[2]
+
+    return xx
+
+# normalize original images
+def norm(x):
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    xx = x.clone().detach().to(xdevice)
+    # xx = torch.zeros(x.shape).to(device)
+    xx[:, 0, :, :] = (x[:, 0, :, :] - mean[0])/std[0]
+    xx[:, 1, :, :] = (x[:, 1, :, :] - mean[1])/std[1]
+    xx[:, 2, :, :] = (x[:, 2, :, :] - mean[2])/std[2]
+
+    return xx
+
+def get_theta(bs = 64):
+    t = torch.rand(bs)*np.pi - np.pi/2
+    theta = torch.zeros(bs, 2, 3)
+    theta[:,0,0] = torch.cos(t)
+    theta[:,0,1] = -torch.sin(t)
+    theta[:,1,0] = torch.sin(t)
+    theta[:,1,1] = torch.cos(t)
+    
+    # translation
+    theta[:,0,2] = torch.rand(bs) - 0.5
+    theta[:,1,2] = torch.rand(bs) - 0.5
+    return theta
+
+def transform(image, theta):
+    bs = image.shape[0]
+    grid = F.affine_grid(theta, image.view(bs,3,128,128).size(), align_corners = True)
+    grid = grid.float().to(image.device)
+    tran = F.grid_sample(image, grid, align_corners = True)
+    tran[tran == 0] = tran.min()
+    return tran
+
+def get_n_params(model):
+    pp=0
+    for p in list(model.parameters()):
+        nn=1
+        for s in list(p.size()):
+            nn = nn*s
+        pp += nn
+    return pp
+
+
+def plot_explanation(img, explanation):
+    fig, ax = plt.subplots(1,2,figsize = (6,3))
+    ax[0].imshow(torch.clamp(denorm(img),0,1).squeeze().detach().cpu().numpy().transpose((1,2,0)))
+    ax[0].axis('off')
+    ax[1].imshow(torch.clamp(denorm(img),0,1).squeeze().detach().cpu().numpy().transpose((1,2,0)))
+    ax[1].imshow(explanation.detach().cpu().numpy(), cmap = 'seismic', alpha = 0.4)
+    ax[1].axis('off')
+    plt.tight_layout()
+    plt.show()
